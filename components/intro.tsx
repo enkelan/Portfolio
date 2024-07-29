b9265be43b461d633be07b9ef456c1e541035361
Eno <eno_rami@ymail.com>
2024-07-29 14:47:36 +0200

    small modification

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
           >
             <Image
               src="/eno.png"
-              alt="Ricardo portrait"
+              alt="Enkelan Rami"
               width="192"
               height="192"
               quality="95"
