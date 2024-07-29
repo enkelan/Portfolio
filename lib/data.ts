70b7f5b6437762b33cd80088ccb4040c532c91c1
Eno <eno_rami@ymail.com>
2024-07-29 14:31:45 +0200

    small modification

--- lib/data.ts
+++ lib/data.ts
@@ -3,8 +3,6 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import HellDivers2 from "@/public/HellDivers2.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
